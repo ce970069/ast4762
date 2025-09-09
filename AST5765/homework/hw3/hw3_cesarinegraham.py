13fcbb0b31a5705da636f7fa21ac593363e9e463
Césarine Graham <>
2025-09-08 22:05:01 -0400

    Added print statements to files

--- AST5765/homework/hw3/hw3_cesarinegraham.py
+++ AST5765/homework/hw3/hw3_cesarinegraham.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 #Cesarine Graham
@@ -9,7 +9,7 @@
 #09/05/2025
 
 
-# In[2]:
+# In[3]:
 
 
 #importing my libraries
@@ -17,28 +17,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[3]:
-
-
-from hw3_cesarinegraham_support_functions import square
-
-
 # In[4]:
 
 
-# example for the support function docstring
-square (8)
+from hw3_cesarinegraham_support_functions import square
 
 
 # In[5]:
 
 
 # example for the support function docstring
+print ( square (8) )
+
+
+# In[7]:
+
+
+# example for the support function docstring
 a = np.array([4, 2, 3])
-square(a)
+print( square(a) )
 
 
-# In[6]:
+# In[8]:
 
 
 # example for the support function docstring
@@ -47,45 +47,45 @@ d = np.array([
     [2, 9, 7],
     [1, 2, 3]
 ])
-square(d)
-
-
-# In[7]:
-
-
-# example for the support function docstring
-square(3.7)
-
-
-# In[8]:
-
-
-# example for the support function docstring
-square(3 + 4j)
+print( square(d) )
 
 
 # In[9]:
 
 
 # example for the support function docstring
-# square("word") #making sure the typeerror works
-#commented this out so i can continue running without an error
+print( square(3.7) )
 
 
 # In[10]:
 
 
+# example for the support function docstring
+print( square(3 + 4j) )
+
+
+# In[9]:
+
+
+# example for the support function docstring
+print( square("word") ) #making sure the typeerror works
+
+
+# In[12]:
+
+
 #problem 2h
 #create an array test_square_1 with integers from 0 to 9. Import and call function square to square array test_square_1 and print the result
+
+from hw3_cesarinegraham_support_functions import square #importing square
 
 test_square_1 = np.arange(10) #array with int from 0 to 10
 print(test_square_1) #checking the array
 
-from hw3_cesarinegraham_support_functions import square #importing square
-square(test_square_1) 
+print( square(test_square_1) )
 
 
-# In[11]:
+# In[13]:
 
 
 # problem 2i
@@ -95,10 +95,10 @@ square(test_square_1)
 test_square_2 = np.linspace(0, 25, 25).reshape(5, 5) #5x5 array w floats from 0 to 25
 print(test_square_2) #checking the array
 
-square(test_square_2) 
+print( square(test_square_2) )
 
 
-# In[12]:
+# In[14]:
 
 
 #problem 3
